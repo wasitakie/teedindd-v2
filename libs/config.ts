699757b2710e4f77ce72a0997e83f229b022bbb0
wasitakie <wasitaya@gmail.com>
2026-02-04 17:34:07 +0700

    deploy to vercel

--- libs/config.ts
+++ libs/config.ts
@@ -8,12 +8,16 @@ const pool =
   globalForMysql.connection ??
   mysql.createPool({
     host: process.env.DB_HOST,
+    port: 4000,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_DATABASE,
     waitForConnections: true,
-    connectionLimit: 10, // จำกัดจำนวนการเชื่อมต่อพร้อมกัน
+    connectionLimit: 5, // จำกัดจำนวนการเชื่อมต่อพร้อมกัน
     queueLimit: 0,
+    ssl: {
+      rejectUnauthorized: true, // TiDB Cloud บังคับ SSL
+    },
   });
 if (process.env.NODE_ENV !== "production") {
   globalForMysql.connection = pool;
