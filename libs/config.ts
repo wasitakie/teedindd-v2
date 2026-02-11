5a9e6c5e24138686ca261e8fa3597f6f2e921475
wasitakie <wasitaya@gmail.com>
2026-02-11 13:33:19 +0700

    deploy to vercel

--- libs/config.ts
+++ libs/config.ts
@@ -1,16 +1,22 @@
 import mysql from "mysql2/promise";
 
 const globalForMysql = global as unknown as {
-  pool: mysql.Pool | undefined;
+  pool?: mysql.Pool;
 };
 
-const pool =
-  globalForMysql.pool ??
-  mysql.createPool({
-    uri: process.env.DATABASE_URL!,
+function createPool() {
+  if (!process.env.DATABASE_URL) {
+    throw new Error("DATABASE_URL is not defined");
+  }
+
+  return mysql.createPool({
+    uri: process.env.DATABASE_URL,
     waitForConnections: true,
     connectionLimit: 5,
   });
+}
+
+export const pool = globalForMysql.pool ?? createPool();
 
 if (process.env.NODE_ENV !== "production") {
   globalForMysql.pool = pool;
